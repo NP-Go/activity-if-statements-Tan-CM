3f8c996e95d45f935730280fd61a9c218a2251ae
Tan-CM <tanchorming@gmail.com>
2022-03-29 11:52:52 +0800

    Coded the requirement for "If"

--- if.go
+++ if.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"fmt"
+	"strconv"
+)
+
 func main() {
 	number1 := 17
 	number2 := 24
@@ -7,4 +12,31 @@ func main() {
 	//Insert your code here
 	//Hint: You may wish to make use of strconv.Itoa to convert int to string
 
+	diff := 0
+	if number1 < number2 {
+		diff = number2 - number1
+		resultMessage = "Number2 is greater than Number1 by " + strconv.Itoa(diff)
+	} else if number1 > number2 {
+		diff = number1 - number2
+		resultMessage = "Number1 is greater than Number2 by " + strconv.Itoa(diff)
+	} else {
+		resultMessage = "Number1 is equal to Number2"
+	}
+	fmt.Println(resultMessage)
+
+	resultMessage = "Number1(" + (strconv.Itoa(number1)) + ") is "
+	if number1%2 == 1 {
+		resultMessage = resultMessage + "odd"
+	} else {
+		resultMessage = resultMessage + "even"
+	}
+	fmt.Println(resultMessage)
+
+	resultMessage = "Number2(" + (strconv.Itoa(number2)) + ") is "
+	if number2%2 == 1 {
+		resultMessage = resultMessage + "odd"
+	} else {
+		resultMessage = resultMessage + "even"
+	}
+	fmt.Println(resultMessage)
 }
